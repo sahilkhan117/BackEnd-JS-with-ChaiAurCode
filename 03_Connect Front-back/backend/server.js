9cf2dfae746eba6035080bd1fd48d1a1eeb4a07f
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-19 14:08:19 +0000

    make a sample JSON

--- 03_Connect Front-back/backend/server.js
+++ 03_Connect Front-back/backend/server.js
@@ -1,3 +1,13 @@
 import express from "express";
 const app = express();
 const PORT = 3000;
+
+const Myjokes = [
+	{id: 1, title: "1st Joke", joke: "Joke One"},
+	{id: 2, title: "2nd Joke", joke: "Joke Two"},
+	{id: 3, title: "3rd Joke", joke: "Joke Three"},
+	{id: 4, title: "4th Joke", joke: "Joke Four"},
+	{id: 5, title: "5th Joke", joke: "Joke Five"},
+	{id: 6, title: "6th Joke", joke: "Joke Six"},
+	{id: 7, title: "7th Joke", joke: "Joke Seven"},
+  ]
