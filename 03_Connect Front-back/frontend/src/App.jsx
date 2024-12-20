ff76d7cde9a4e0a061a8a8ac660c14e97bc97180
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-20 16:02:01 +0000

    that is recieve data from my backend

--- 03_Connect Front-back/frontend/src/App.jsx
+++ 03_Connect Front-back/frontend/src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
 	const [jokes, setJokes] = useState([]);
 
-	
+	useEffect(() => {
+		fetch("/api/message")
+			.then((response) => response.json())
+			.then((data) => setJokes(data));
+	});
 
 	return (
 		
