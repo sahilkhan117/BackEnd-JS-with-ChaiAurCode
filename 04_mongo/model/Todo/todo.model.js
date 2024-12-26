bb48bb2e5247dd3e33e50a97739143c5ff1ded25
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-26 05:26:38 +0000

    write sample schema for todo

--- 04_mongo/model/Todo/todo.model.js
+++ 04_mongo/model/Todo/todo.model.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema(
-    {
-        
-    }
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		completed: {
+			type: Boolean,
+			default: false,
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		subTodo: [  // Array of sub Todos
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "SubTodo",
+			},
+		],
+	},
+	{ timeStamp: true }
 );
 
 export const Todo = mongoose.model("Todo", todoSchema);
