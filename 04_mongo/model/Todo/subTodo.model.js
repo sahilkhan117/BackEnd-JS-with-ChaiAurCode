c7d2896b525873412d20ed9181e833882f12b3ae
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-29 05:41:23 +0000

    create subTodo schema

--- 04_mongo/model/Todo/subTodo.model.js
+++ 04_mongo/model/Todo/subTodo.model.js
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 
-const SubTodoSchema = new mongoose.Schema({
-    
-});
+const SubTodoSchema = new mongoose.Schema(
+	{
+		content: {
+			type: String,
+			required: true,
+		},
+		completed: {
+			type: Boolean,
+			default: false,
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{ timestamps: true }
+);
 
 export const SubTodo = mongoose.model("SubTodo", SubTodoSchema);
